a41579765623710901d46399b0ad2340d5ad46c5
Robin Brooke <rpbrooke@gmail.com>
2017-08-13 19:54:39 -0400

    First Commit

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,39 @@
+CREATE TABLE IF NOT EXISTS league
+( league_position int(1) not null,
+  club_name VARCHAR(25) not null,
+  gamesplayed int(2),
+  wins int(2),
+  losses int(2),
+  ties int(2),
+  PRIMARY KEY (club_name),
+);
+
+CREATE TABLE IF NOT EXISTS clubs
+( club_id int(10) not null AUTO_INCREMENT,
+  club_name VARCHAR(25) not null,
+  PRIMARY KEY (club_id),
+  FOREIGN KEY (club_name) REFERENCES league(club_name)
+);
+
+CREATE TABLE IF NOT EXISTS players
+(
+  player_name VARCHAR(25) NOT NULL,
+  player_number int(2),
+  club_id int(10),
+  PRIMARY KEY (player_name),
+  FOREIGN KEY (club_id) REFERENCES clubs(club_id)
+);
+
+CREATE TABLE IF NOT EXISTS matches
+( match_id  int(10) not null AUTO_INCREMENT,
+  home_team VARCHAR(25) not null,
+  away_team VARCHAR(25) not null,
+  winning_team VARCHAR(25) not null,
+  home_score int(2) not null,
+  away_score int(2) not null,
+  PRIMARY KEY (match_id),
+  FOREIGN KEY (home_team) REFERENCES clubs(club_name),
+  FOREIGN KEY (away_team) REFERENCES clubs(club_name),
+  FOREIGN KEY (winning_team) REFERENCES clubs(club_name)
+);
+
---
+++ target/classes/data.sql
@@ -0,0 +1,31 @@
+INSERT INTO league (league_position, club_name, club_gamesplayed, club_wins, club_losses, club_ties) VALUES
+  (1,'Arsenal',1,1,0,0),
+  (5,'Chelsea',2,0,2,0),
+  (1,'TeamSoloMid',1,1,0,0),
+  (1, 'Cloud9',2,1,1,0),
+  (1, 'SKTelecom',1,1,0,0);
+
+INSERT INTO clubs (club_name) VALUES
+  ('Arsenal'),
+('Chelsea'),
+('TeamSoloMid'),
+('Cloud9'),
+('SKTelecom');
+
+INSERT INTO players (player_name, player_number, club_id) VALUES
+  ('Tim', 12,'1'),
+  ('Jim', 10,'1'),
+  ('Kim', 12,'2'),
+  ('Pim', 11,'2'),
+  ('Mim', 8,'3'),
+('Xim', 1,'3'),
+('Wim', 2,'4'),
+  ('Zim', 7,'4');
+
+
+
+INSERT INTO matches(home_team, away_team, winning_team, home_score, away_score) VALUES
+  ('Arsenal','Chelsea','Arsenal',2,0),
+  ('Chelsea', 'Cloud9', 'Cloud9', 1,2),
+  ('TeamSoloMid','Chelsea','TeamSoloMid',1,0),
+  ('SKTelecom', 'Cloud9', 'SKTelecom', 0,2);
